457606fffad2cd63efc57325290e64b838a95fa6
unknown <engraaadeyemi@gmail.com>
2022-09-30 10:53:58 +0100

    fetch api

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> | any = (
+  const [data, setData] = useState<[] | any>([]);
+  const [url, setUrl] = useState<string>("");
+
+  const api = `https://api.shrtco.de/v2/shorten?url=${url}`;
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> | any = async (
     e: React.FormEvent<HTMLFormElement>
   ) => {
     e.preventDefault();
+    try {
+      if (url === "") {
+        alert("Input Field is Empty");
+      } else {
+        const response = await fetch(api);
+        const data = await response.json();
+        if (data.ok === false) {
+          alert("Invalid Url");
+        }
+        setData(data);
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -14,6 +34,8 @@ const Search = () => {
           type="text"
           className="w-full h-full pl-6 py-3 rounded-lg outline-none"
           placeholder="Shorten a link here..."
+          onChange={(e) => setUrl(e.target.value)}
+          value={url}
         />
       </form>
       <div
