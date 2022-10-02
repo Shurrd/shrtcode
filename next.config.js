fc71ffa226b43b2e6cb43f57c87acbbd8fe6aca7
unknown <engraaadeyemi@gmail.com>
2022-10-02 15:03:31 +0100

    swc

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
-  swcMinify: false,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
